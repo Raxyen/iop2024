0a3db7237d1a3f0950905fdc07552bdbfc03cbfc
raxyen <tanzanitena@gmail.com>
2025-01-29 16:01:27 +0100

    Pierwszy commit - dodanie pliku podzielnosc.cpp

---
+++ podzielnosc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int a, b;
+    cout << "Wprowadź dwie liczby całkowite: ";
+    cin >> a >> b;
+    cout << "Pierwsza liczba: " << a << ", Druga liczba: " << b << std::endl;
+    return 0;
+}
+
